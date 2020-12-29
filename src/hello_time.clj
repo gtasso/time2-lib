3924eefbed173b2d30443f8cf44615d8809005a9
George Tasso <getasso@gmail.com>
2020-12-29 17:03:55 +1100

    init

--- src/hello_time.clj
+++ src/hello_time.clj
@@ -7,7 +7,7 @@
   (t/instant))
 
 (defn time-str
-  "Returns a string representation of a datetime in the local time zone."
+  "xsReturns a string repressddentation of a datetime in the local time zone."
   [instant]
   (t/format
     (t/with-zone (t/formatter "hh:mm a") (t/zone-id))
